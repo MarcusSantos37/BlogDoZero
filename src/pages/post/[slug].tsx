6fb73a6f4eb498b1f962fb096a9662a91791c5ce
MarcusAMS <marcusastrade@gmail.com>
2022-02-23 21:36:19 -0300

    finalizando features

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -21,9 +21,11 @@ import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -41,13 +43,29 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  navigation: {
+    prevPost: {
+      uid: string;
+      data: {
+        title: string;
+      }[];
+    };
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      }[];
+    };
+  };
   preview: boolean;
 }
 
-export default function Post({ post, preview }: PostProps): JSX.Element {
+export default function Post({
+  post,
+  preview,
+  navigation,
+}: PostProps): JSX.Element {
   const router = useRouter();
-
-  console.log('preview', preview);
 
   if (router.isFallback) {
     return <h1>Carregando...</h1>;
@@ -63,6 +81,18 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
     'dd MMM yyyy',
     { locale: ptBR }
   );
+
+  const isPostEdited =
+    post.first_publication_date !== post.last_publication_date;
+
+  let editionDate;
+  if (isPostEdited) {
+    editionDate = format(
+      new Date(post.last_publication_date),
+      "'* editado em' dd MMM yyyy', às' H':'m",
+      { locale: ptBR }
+    );
+  }
 
   return (
     <>
@@ -89,6 +119,8 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
                 {readTime} min
               </li>
             </ul>
+
+            {isPostEdited && <span>{editionDate}</span>}
           </div>
           {post.data.content.map(content => {
             return (
@@ -104,6 +136,29 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             );
           })}
         </div>
+
+        <section className={`${styles.navigation} ${commonStyles.container}`}>
+          {navigation?.prevPost.length > 0 && (
+            <div>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+
+          {navigation?.nextPost.length > 0 && (
+            <div>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+
+        <Comments />
+
         {preview && (
           <aside>
             <Link href="/api/exit-preview">
@@ -147,9 +202,28 @@ export const getStaticProps: GetStaticProps = async ({
     ref: previewData?.ref || null,
   });
 
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+    }
+  );
+
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -167,6 +241,10 @@ export const getStaticProps: GetStaticProps = async ({
   };
 
   return {
-    props: { post, preview },
+    props: {
+      post,
+      navigation: { prevPost: prevPost?.results, nextPost: nextPost?.results },
+      preview,
+    },
   };
 };
